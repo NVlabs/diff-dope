14282b9df977ebfcc7e1222e0296cc894be014d9
Jonathan Tremblay <tonton.tremblay@gmail.com>
2023-09-27 16:13:46 -0700

    adding some thoughts on how we are going to use the interface

--- examples/simple_scene_draft.py
+++ examples/simple_scene_draft.py
@@ -5,30 +5,45 @@ import hydra
 
 @hydra.main(version_base=None,config_path="../configs/",config_name='diffdope')
 def main(cfg: DictConfig):
+
+	# create diff-dope to optimize
 	d_dope = dd.DiffDope(cfg = cfg)
 
+	# a new camera
 	cam = dd.Camera(100,100,50,50,100,100)
+
+	# a new pose
 	pose = dd.Pose([1,1,1],[0,0,0,1])
 
+	# load the hyper params from the config file
 	hyperparameters = dd.Params(**cfg.parameters)
 
+	# load a mesh data
 	mesh = dd.Mesh('path')
 
+	# load the data for a scene to optimize
 	images = dd.Images(path_rgb = '',path_depth = '',path_segmentation = '')
 
+	# update diff dope to have all this new data. 
 	d_dope.camera = cam 
 	d_dope.pose = pose 
 	d_dope.mesh = mesh
 	d_dope.images = images
 	d_dope.hyperparameters = hyperparameters
 
+	# put things on the gpu
 	d_dope.to_cuda()
 
+	# run the optimization and get the best pose
 	pose = d_dope.optimize()
 
+	# render an image from the optimization that was run
 	image = dd.render_pose(d_dope,overlay = True,alpha = 10,path='image1.png')
+
+	# render the optimization visualization for the the best pose
 	dd.render_video_optimization(d_dope,path='my_vide0.mp4',fps=24)
 	
+	# chanse some hyperparams to play with the diff-dope
 	d_dope.hyperparameters.batchsize = 64
 	d_dope.hyperparameters.learning_rates = [0.1,10]
 	d_dope.hyperparameters.weights['rgb'] = 100
@@ -39,14 +54,15 @@ def main(cfg: DictConfig):
 	pose = d_dope.optimize()
 
 
-	d_dope.pose = Pose([10,10,10],[1,0,0,0])
-	pose = d_dope.optimize()
 
 	# came from a different place than a file 
-	t = torch.zeros(3).cuda().grad()
-	q = torch.zeros(4).cuda().grad()
+	#  could be a diff pnp 
+	pose_from_pnp = super_process_pnp()
 
-	d_dope.pose = Pose(t,q)
+	d_dope.pose = Pose(
+		pose_from_pnp['position'],
+		pose_from_pnp['rotation']
+	)
 	pose = d_dope.optimize()
 
 

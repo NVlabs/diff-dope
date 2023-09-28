c5daa07705d3ba2bff80f904f7a8c6fd8b5ee69b
Stephen Tyree <styree@nvidia.com>
2023-09-28 10:57:08 -0500

    added comment with possible requirements for nvdiffrast

--- setup.py
+++ setup.py
@@ -12,6 +12,12 @@ setup(
         "pyrr",
         "torch",
         "trimesh",
+        # The following might be requirements for nvdiffrast
+        # (they are installed by pip in the Dockerfile),
+        # but we should check if they are really needed:
+        # "ninja",
+        # "imageio",
+        # "imageio-ffmpeg",
     ],
     extras_require={
         "dev": [

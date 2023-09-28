497fe5d7bb4a4c4ccd5664c9c42eef6236c560eb
Stephen Tyree <styree@nvidia.com>
2023-09-28 10:49:35 -0500

    added requried and dev dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='diffdope',
-    version='0.1',
+    name="diffdope",
+    version="0.1",
     packages=find_packages(),
+    install_requires=[
+        "hydra-core",
+        "numpy",
+        "nvdiffrast@git+https://github.com/NVlabs/nvdiffrast.git",
+        "omegaconf",
+        "pyrr",
+        "torch",
+        "trimesh",
+    ],
+    extras_require={
+        "dev": [
+            "black",
+            "ipython",
+            "pre-commit",
+            "pylint",
+            "pytest",
+        ],
+    },
 )
